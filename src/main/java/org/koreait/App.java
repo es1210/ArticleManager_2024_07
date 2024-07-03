7489fe7ca5c27380ca4c5d9b30862ae21424438d
es1210 <dmstn27034@gmail.com>
2024-07-03 14:18:04 +0900

    App 간소화

--- src/main/java/org/koreait/App.java
+++ src/main/java/org/koreait/App.java
@@ -12,6 +12,8 @@ public class App {
         articleController.makeTestData();
         memberController.makeTestData();
 
+        Controller controller = null;
+
         while (true) {
             System.out.print("명령어) ");
             String cmd = sc.nextLine().trim();
@@ -22,27 +24,48 @@ public class App {
             if (cmd.equals("exit")) {
                 break;
             }
-            if (cmd.equals("member join")) {
-                memberController.doJoin();
-            } else if (cmd.equals("article write")) {
+            String[] cmdBits = cmd.split(" ");
 
-                articleController.doWrite();
-            } else if (cmd.startsWith("article list")) {
-                articleController.showList(cmd);
-            } else if (cmd.startsWith("article detail")) {
-                articleController.showDetail(cmd);
-            } else if (cmd.startsWith("article delete")) {
-                articleController.doDelete(cmd);
-            } else if (cmd.startsWith("article modify")) {
-                articleController.doModify(cmd);
+            String controllerName = cmdBits[0];
+
+            if (cmdBits.length == 1) {
+                System.out.println("명령어 확인해");
+                continue;
+            }
+
+            String actionMethodName = cmdBits[1];
+
+            if (controllerName.equals("article")) {
+                controller = articleController;
+            } else if (controllerName.equals("member")) {
+                controller = memberController;
             } else {
                 System.out.println("사용할 수 없는 명령어입니다");
+                continue;
             }
+            controller.doAction(cmd, actionMethodName);
+
+//            if (cmd.equals("member join")) {
+//                memberController.doJoin();
+//            } else if (cmd.equals("article write")) {
+//                articleController.doWrite();
+//            } else if (cmd.startsWith("article list")) {
+//                articleController.showList(cmd);
+//            } else if (cmd.startsWith("article detail")) {
+//                articleController.showDetail(cmd);
+//            } else if (cmd.startsWith("article delete")) {
+//                articleController.doDelete(cmd);
+//            } else if (cmd.startsWith("article modify")) {
+//                articleController.doModify(cmd);
+//            } else {
+//                System.out.println("사용할 수 없는 명령어입니다");
+//            }
+
         }
         System.out.println("==프로그램 종료==");
         sc.close();
     }
 }
-            }
+
 
 
